3adfc42afe09c1d46c742a0b31017149f2c92874
licsman <miaojw@163.com>
2020-05-21 16:22:45 +0800

    create project

---
+++ src/test/java/com/geekstudy/orange/OrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekstudy.orange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
