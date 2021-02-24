2c9a889882ebc107d95d07ca932be6413a6a5144
licsman <miaojw@163.com>
2021-02-24 17:56:47 +0800

    login module

--- src/main/java/com/geekstudy/orange/controller/v1/TeacherController.java
+++ src/main/java/com/geekstudy/orange/controller/v1/TeacherController.java
@@ -1,19 +1,26 @@
 package com.geekstudy.orange.controller.v1;
 
 import com.geekstudy.orange.db.model.GeekTeacher;
+import com.geekstudy.orange.entity.LoginForm;
 import com.geekstudy.orange.service.teacher.TeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Map;
 
 @Api(value = "TeacherController", description = "Restful api to query/get/create/delete teacher")
 @RestController("v1.TeacherController")
 @RequestMapping("/v1/teacher")
 public class TeacherController {
     private final TeacherService teacherService;
+
+    private static final Logger logger = LoggerFactory.getLogger(TeacherController.class);
 
     public TeacherController(TeacherService teacherService) {
         this.teacherService = teacherService;
@@ -43,5 +50,26 @@ public class TeacherController {
         geekTeacher.setTeacherId(teacherId);
         return teacherService.updateTeacher(geekTeacher);
     }
+
+    @ApiOperation(value = "Login Manage System", notes = "Login System")
+    @PostMapping(value = "login/{userId}")
+    public String login( @PathVariable int userId, HttpSession httpSession) {
+        if (httpSession.isNew()){
+            GeekTeacher geekTeacher = teacherService.queryTeacher(userId);
+            httpSession.setAttribute("userInfo", geekTeacher);
+            return "第一次登录：" + "用户信息:" + geekTeacher;
+        } else {
+            GeekTeacher geekTeacher = (GeekTeacher) httpSession.getAttribute("userInfo");
+            return "欢迎回来：" + "从session中读取到的用户信息:" + geekTeacher;
+        }
+    }
+
+    @ApiOperation(value = "administrator login")
+    @PostMapping(value = "adminLogin", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String adminLogin(@RequestBody LoginForm loginInfo) {
+        logger.info(String.valueOf(loginInfo));
+        return "success";
+    }
+
 
 }
---
+++ src/main/java/com/geekstudy/orange/entity/LoginForm.java
@@ -0,0 +1,40 @@
+package com.geekstudy.orange.entity;
+
+public class LoginForm {
+    private String userName;
+    private String password;
+    private String verCode;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVerCode() {
+        return verCode;
+    }
+
+    public void setVerCode(String verCode) {
+        this.verCode = verCode;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", verCode='" + verCode + '\'' +
+                '}';
+    }
+}
